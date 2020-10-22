8a1b2f3e9da6c835e53053a6ae251a269735be6e
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2020-10-22 17:35:43 +0100

    feat: add an example with generators

--- broadcast-reactive.spec.js
+++ broadcast-reactive.spec.js
@@ -1,4 +1,5 @@
 const rx = require("rxjs");
+const { toArray } = require("rxjs/operators");
 const { broadcastEventsInOrder } = require("./broadcast-reactive");
 
 it("should handle all passed events", async () => {
@@ -94,4 +95,23 @@ it("should preserve the whitelist order", async () => {
   eventSource.complete();
 
   expect(publishedMessages).toEqual(["a", "b", "d"]);
+});
+
+it("should support generators", async () => {
+  function* gen() {
+    const items = ["a", "b", "xxxx", "b", "d", "b", "c", "c", "d", "a"];
+    while (items.length) {
+      yield items.shift();
+    }
+  }
+
+  const order = ["a", "b", "c"];
+  const iterator = gen();
+  const eventSource = rx.from(iterator);
+
+  const messagesToPublish = broadcastEventsInOrder(order, eventSource);
+
+  const publishedMessages = await messagesToPublish.pipe(toArray()).toPromise();
+
+  expect(publishedMessages).toEqual(["a", "b", "c"]);
 });
